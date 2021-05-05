572c21e6f9549cb09e4d7ddb5bd236f488590bd4
steve51516 <steve51516@gmail.com>
2021-05-05 19:44:08 +0000

    move parse_config function to main

--- fan_monitor.py
+++ fan_monitor.py
@@ -1,39 +1,14 @@
-from yaml import safe_load
 from time import sleep
 from subprocess import Popen, PIPE
 import smbus, logging
 
 class FanMonitor:
-    def __init__(self):
-        self.fanconfig = {
-            'temperatures': {
-                65: 100,
-                60: 55,
-                55: 10
-            },
-            'loglevel': {'DEBUG'}
-        }
-        try:        
+    def __init__(self, config):
+        self.fanconfig = config
+        try:
             self.bus = smbus.SMBus(1)
         except:
             self.bus = smbus.SMBus(0)
-
-    def parse_config(self):
-        try:
-            logging.info("Reading fand.yaml.")
-            with open('fand.yaml', 'r') as file:
-                config = safe_load(file)
-            logging.info("Successfully read fand.yaml.")
-            if len(config) == 0:
-                return self.fanconfig
-            else:
-                loglevel = f"logging.{config['loglevel']}"
-                logging.basicConfig(format='%(asctime)s - %(message)s', level=loglevel)
-                return config
-        except Exception as e:
-            logging.error(f"Could not read fand.yaml, error message: {e}")
-            logging.error("Using values of 65=100, 60=55, 55=10")
-            return self.fanconfig
         
     def compare_fanspeed(self, temperature, fanconfig):
         for temp in fanconfig['temperatures']:
@@ -57,7 +32,8 @@ class FanMonitor:
     def fan_monitor(self):
         address = 0x1a
         block = 0
-        self.fanconfig = self.parse_config()
+        loglevel = f"logging.{self.fanconfig['loglevel']}"
+        logging.basicConfig(format='%(asctime)s - %(message)s', level=loglevel)
         while True:
             tempC = self.read_temperature()
             block = self.compare_fanspeed(tempC, self.fanconfig)
--- main.py
+++ main.py
@@ -3,22 +3,51 @@ from fan_monitor import FanMonitor
 from powerbutton import PowerButton
 from signal import signal, SIGINT
 from sys import exit
+from yaml import safe_load
+import logging
+
+default_fanconfig = {
+        'temperatures': {
+            65: 100,
+            60: 55,
+            55: 10
+        },
+        'loglevel': {'DEBUG'}
+    }
 
 def handler(signal_received, frame):
-    print('SIGINT or CTRL-C detected. Exiting gracefully')
+    logging.info('SIGINT or CTRL-C detected. Exiting gracefully')
     exit(0)
+
+def parse_config():
+    try:
+        logging.info("Reading fand.yaml.")
+        with open('fand.yaml', 'r') as file:
+            config = safe_load(file)
+        logging.info("Successfully read fand.yaml.")
+        if len(config) == 0:
+            return default_fanconfig
+        else:
+            return config
+    except Exception as e:
+        logging.error(f"Could not read fand.yaml, error message: {e}")
+        logging.error("Using values of 65=100, 60=55, 55=10")
+        return default_fanconfig
 
 if __name__=="__main__":
     signal(SIGINT, handler)
-    fan_monitor = FanMonitor()
+    config = parse_config()
+    fan_monitor = FanMonitor(config)
+    loglevel = f"logging.{config['loglevel']}"
+    logging.basicConfig(format='%(asctime)s - %(message)s', level=loglevel)
     power_button = PowerButton()
     th_fan_monitor = Thread(target=fan_monitor.fan_monitor(), daemon=True)
     th_power_button = Thread(target=power_button.monitor(), daemon=True)
     try:
-        print("Starting fan monitoring thread now.")
+        logging.info("Starting fan monitoring thread now.")
         th_fan_monitor.start()
-        print("Starting powerbutton monitoring thread now.")
+        logging.info("Starting powerbutton monitoring thread now.")
         th_power_button.start()
     except Exception as e:
-        print(e)
-        print("Stopping threads now...")
+        logging.error(f"An Exception occured while starting threads: {e}")
+        logging.error("Stopping threads now...")
