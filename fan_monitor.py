8418effee447a02a18afc5915b2b19003c315d94
steve51516 <steve51516@gmail.com>
2021-05-05 18:49:18 +0000

    enabled logging

--- fan_monitor.py
+++ fan_monitor.py
@@ -1,7 +1,7 @@
 from yaml import safe_load
 from time import sleep
 from subprocess import Popen, PIPE
-import smbus
+import smbus, logging
 
 class FanMonitor:
     def __init__(self):
@@ -17,23 +17,23 @@ class FanMonitor:
 
     def parse_config(self):
         try:
-            print("Reading fand.yaml.")
+            logging.info("Reading fand.yaml.")
             with open('fand.yaml', 'r') as file:
                 config = safe_load(file)
-            print("Successfully read fand.yaml.")
+            logging.info("Successfully read fand.yaml.")
             if len(config) == 0:
                 return self.fanconfig
             else:
                 return config
         except Exception as e:
-            print(f"Could not read fand.yaml, error message: {e}")
-            print("Using values of 65=100, 60=55, 55=10")
+            logging.error(f"Could not read fand.yaml, error message: {e}")
+            logging.error("Using values of 65=100, 60=55, 55=10")
             return self.fanconfig
         
     def compare_fanspeed(self, temperature, fanconfig):
         for temp in fanconfig:
             if temp >= temperature:
-                return temp
+                return fanconfig[temp]
         return 0
     
     def read_temperature(self):
@@ -41,10 +41,10 @@ class FanMonitor:
             p1 = Popen(['vcgencmd', 'measure_temp'], stdout=PIPE)
             data = p1.communicate()
         except Exception as e:
-            print(e)
-            
+            logging.error(f"Error running vcgencmd measure_temp: {e}")
         if data[1] is not None:
-            print(f"Error running vcgencmd measure_temp: {data[1]}")
+            logging.error(f"Error running vcgencmd measure_temp: {data[1]}")
+
         str_tempC = data[0]
         str_tempC = str_tempC.replace("temp=","")
         return float(str_tempC.replace("\'C",""))
@@ -56,8 +56,9 @@ class FanMonitor:
         while True:
             tempC = self.read_temperature()
             block = self.compare_fanspeed(tempC, self.fanconfig)
+            logging.info(f"Current CPU temperature\n\tC:{tempC}\n\tF:{tempC * 1.8 + 32}\nSetting Fan speed to: {block}")
             try:
                 self.bus.write_byte(address, block)
             except IOError as e:
-                print(f"Unable to set fan speed: {e}")
+                logging.error(f"Unable to set fan speed: {e}")
             sleep(30)
