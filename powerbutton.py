e30e6354689be9d9568cf14424fdb89a6014544f
steve51516 <steve51516@gmail.com>
2021-05-05 18:25:17 +0000

    only import call from subprocess

--- powerbutton.py
+++ powerbutton.py
@@ -1,15 +1,15 @@
 from gpiozero import Button
-import subprocess
+from subprocess import call
 
 class PowerButton:
     def __init__(self, button=Button(4)):
         self.button = button
     
     def shutdown(self):
-        subprocess.call("systemctl poweroff")
+        call("systemctl poweroff")
     
     def reboot(self):
-        subprocess.call("systemctl reboot")
+        call("systemctl reboot")
     
     def monitor(self):
         self.button.when_held = self.shutdown
