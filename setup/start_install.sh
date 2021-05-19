98a03fc7b02842b9accd6fcd88d3f4a41e07d20e
steve51516 <steve51516@gmail.com>
2021-05-19 14:36:20 +0000

    directly pass arguements to main.sh

--- setup/start_install.sh
+++ setup/start_install.sh
@@ -1,7 +1,1 @@
-if [[ "${1}" = "--uninstall" ]]; then
-    /bin/bash -c "source setup_settings.env; exec /bin/bash main.sh --uninstall"
-elif [[ "${1}" == "" ]]; then
-    /bin/bash -c "source setup_settings.env; exec /bin/bash main.sh"
-else
-    /bin/bash -c "source setup_settings.env; exec /bin/bash main.sh --usage"
-fi
+/bin/bash -c "source setup_settings.env; exec /bin/bash main.sh ${1}"
